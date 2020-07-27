2980c3f4fc71ffe7415f05e93985b98e723048bd
Goran Jorgic <goran.jorgic@factory.hr>
2020-07-27 07:44:18 +0200

    move twig rendering function into BaseController

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -3,6 +3,11 @@
 namespace ExampleApp\Controller;
 
 use Psr\Http\Message\ResponseInterface;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+use Twig\Loader\FilesystemLoader;
 
 class BaseController
 {
@@ -13,5 +18,25 @@ class BaseController
     )
     {
         $this->response = $response->withHeader('Content-Type', 'text/html');
+    }
+
+    protected function render(string $template, array $data = [])
+    {
+        $loader = new FilesystemLoader([$GLOBALS['viewDir'], $GLOBALS['layoutDir']]);
+        $twig = new Environment($loader);
+
+        try {
+            $this->response->getBody()->write(
+                $twig->render($template, $data)
+            );
+        } catch (LoaderError $e) {
+            $this->response->getBody()->write($e->getMessage());
+        } catch (RuntimeError $e) {
+            $this->response->getBody()->write($e->getMessage());
+        } catch (SyntaxError $e) {
+            $this->response->getBody()->write($e->getMessage());
+        }
+
+        return $this->response;
     }
 }
